b8fe4946f367813d6a9913357898608c06966a46
dgerike <dennis.gerike+github@gmail.com>
2020-12-30 12:45:57 +0100

    load and display all sessions of the selected racing event

--- popup.js
+++ popup.js
@@ -7,8 +7,6 @@ window.onload = function () {
         const url_pattern = 'dtm.com';
 
         if (currentUrl.includes(url_pattern)) {
-            fetchRacingEventsFromMoreCars();
-
             chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
                 const activeTab = tabs[0];
                 chrome.tabs.sendMessage(activeTab.id, {
@@ -32,22 +30,27 @@ function renderList(results) {
     results.forEach(result => {
         html +=
             '<li class="list-group-item">' +
-                '<b>' + result.position +
-                    '. ' + result.driver_name +
-                    ' <small>' + result.team_name + '</small>' +
-                '</b>' +
-                '<span class="badge badge-danger float-right">' + result.points + ' points' + '</span>' +
-                '<br>' +
-                'Total time: ' + result.race_time +
-                ' <small class="float-right">Fastest lap: ' + result.fastest_lap_time + '</small>' +
+            '<b>' + result.position +
+            '. ' + result.driver_name +
+            ' <small>' + result.team_name + '</small>' +
+            '</b>' +
+            '<span class="badge badge-danger float-right">' + result.points + ' points' + '</span>' +
+            '<br>' +
+            'Total time: ' + result.race_time +
+            ' <small class="float-right">Fastest lap: ' + result.fastest_lap_time + '</small>' +
             '</li>';
     });
 
     return html;
 }
 
-function fetchRacingEventsFromMoreCars() {
-    let endpoint = '/racing-series/661/racing-events';
+$('#racingSeriesList').change(function () {
+    let value = $(this).val();
+    fetchRacingEventsFromMoreCars(value);
+});
+
+function fetchRacingEventsFromMoreCars(racingSeriesId) {
+    let endpoint = '/racing-series/' + racingSeriesId + '/racing-events';
 
     $.ajax({
         type: 'GET',
@@ -63,18 +66,52 @@ function fetchRacingEventsFromMoreCars() {
 function renderEventList(events) {
     let html = '';
 
-    html += '<select class="form-control">';
+    html += '<option selected disabled>--- Select Racing Event ---</option>'
     events.forEach(event => {
         html +=
-            '<option>' +
-                'Round ' + event.round +
-                ': ' + event.name +
-                ' (' + event.start_date +
-                ' - ' + event.end_date +
-                ')' +
+            '<option value="' + event.id + '">' +
+            'Round ' + event.round +
+            ': ' + event.name +
+            ' (' + event.start_date +
+            ' - ' + event.end_date +
+            ')' +
             '</option>'
     });
-    html += '</select>';
+
+    return html;
+}
+
+$('#racingEventList').change(function () {
+    let value = $(this).val();
+    fetchRacingEventSessionsFromMoreCars(value);
+});
+
+function fetchRacingEventSessionsFromMoreCars(eventId) {
+    let endpoint = '/racing-events/' + eventId + '/racing-event-sessions';
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://more-cars.net/api/v1' + endpoint,
+    }).done(function (response) {
+        let renderedSessionList = renderSessionsList(response)
+        $('#racingEventSessionsList').html(renderedSessionList);
+    }).fail(function (response, status) {
+
+    });
+}
+
+function renderSessionsList(sessions) {
+    let html = '';
+
+    html += '<option selected disabled>--- Select Session ---</option>'
+    sessions.forEach(session => {
+        html +=
+            '<option value="' + session.id + '">' +
+            session.name +
+            ' - ' + session.duration + ' ' + session.duration_unit +
+            ' (' + session.start_time + ')' +
+            '</option>'
+    });
 
     return html;
 }
