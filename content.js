e81b86d85ac8446c792e759bc6adb45fe0042252
dgerike <dennis.gerike+github@gmail.com>
2021-01-22 14:56:20 +0100

    collect data depending on the session type (race, qualifying or practice)

--- content.js
+++ content.js
@@ -1,19 +1,43 @@
-function extractResultsForPosition(pos) {
-    const baseSelector = 'div.results__content.view table:last-child tr:nth-child(' + pos + ')';
+function detectSessionType() {
+    let pointsColumn = document.querySelector('div.results__content.view th:nth-child(10)')
+    let lapsColumn = document.querySelector('div.results__content.view th:nth-child(9)')
 
+    if (pointsColumn && pointsColumn.textContent === 'Punkte') {
+        return 'race'
+    }
+
+    if (lapsColumn && lapsColumn.textContent === 'runden') {
+        return 'practice'
+    }
+
+    return 'qualifying'
+}
+
+function extractResultsForPosition(pos) {
+    let sessionType = detectSessionType()
+
+    const baseSelector = 'div.results__content.view table:last-child tr:nth-child(' + pos + ')';
     const position = document.querySelector(baseSelector + ' td:nth-child(1)').textContent;
     const driverFirstName = document.querySelector(baseSelector + ' td:nth-child(4)').childNodes[0].textContent;
     const driverLastName = document.querySelector(baseSelector + ' td:nth-child(4)').childNodes[1].textContent;
     const teamName = document.querySelector(baseSelector + ' td:nth-child(4)').childNodes[3].textContent;
-    const raceTime = document.querySelector(baseSelector + ' td:nth-child(7)').textContent;
-    const fastestLap = document.querySelector(baseSelector + ' td:nth-child(8)').textContent;
-    const points = document.querySelector(baseSelector + ' td:nth-child(10)').textContent;
+    let raceTime = null
+    let fastestLap = null
+    let points = null
+
+    if (sessionType === 'race') {
+        raceTime = document.querySelector(baseSelector + ' td:nth-child(7)').textContent.trim();
+        fastestLap = document.querySelector(baseSelector + ' td:nth-child(8)').textContent;
+        points = document.querySelector(baseSelector + ' td:nth-child(10)').textContent;
+    } else {
+        fastestLap = document.querySelector(baseSelector + ' td:nth-child(7)').textContent;
+    }
 
     return {
         "position": parseInt(position),
         "driver_name": driverFirstName + driverLastName,
         "team_name": teamName,
-        "race_time": raceTime.trim(),
+        "race_time": raceTime,
         "fastest_lap_time": fastestLap,
         "points": parseInt(points),
     }
@@ -55,10 +79,12 @@ function extractAllResultsForTheSelectedSession() {
     let resultsCount = document.querySelectorAll('div.results__content.view table:last-child tbody tr').length
     for (let i = 1; i <= resultsCount; i++) {
         let result = extractResultsForPosition(i);
-        if (i === 1) {
-            result.race_time = convertRaceTime(result.race_time);
-        } else {
-            result.race_time = convertRaceTime(result.race_time, results[0].race_time);
+        if (result.race_time) {
+            if (i === 1) {
+                result.race_time = convertRaceTime(result.race_time);
+            } else {
+                result.race_time = convertRaceTime(result.race_time, results[0].race_time);
+            }
         }
         results.push(result)
     }
